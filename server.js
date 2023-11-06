5409945159551e0d9830d67ae6777864038e20bd
Ian Sills <iansills04@gmail.com>
2023-11-06 15:40:39 -0600

    got handlebar working

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const session = require('express-session');
 const routes = require('./controllers');
+const path = require('path');
+const exphbs = require('express-handlebars');
+
+
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
@@ -18,13 +22,21 @@ const sess = {
   })
 };
 
+
+
+const hbs = exphbs.create({  });
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 app.use(session(sess));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, () => console.log('Now listening at http://localhost:3001'));
 });
