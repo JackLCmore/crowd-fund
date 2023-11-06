d914072bc2e3618a6e415282939c7006fc794079
Ian Sills <iansills04@gmail.com>
2023-11-06 17:35:53 -0600

    nothing

--- controllers/homepage.js
+++ controllers/homepage.js
@@ -12,7 +12,7 @@ router.get('/', async (req,res) => {
 
 
 router.get('/login', (req, res) => {
-    if (req.session.loggedIn) {
+    if (req.session.logged_in) {
       res.redirect('/');
       return;
     }
